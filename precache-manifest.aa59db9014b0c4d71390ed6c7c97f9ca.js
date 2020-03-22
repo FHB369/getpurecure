fd5abe87a774bc5ff178a08729cc9a6afefd92cc
Faisal Haque Bappy <hbfaisal66@gmail.com>
2020-03-22 21:00:43 +0600

    Updates

---
+++ precache-manifest.aa59db9014b0c4d71390ed6c7c97f9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b0b6ad1fb97aaf8191cfb4e4acccbd",
+    "url": "/getpurecure/index.html"
+  },
+  {
+    "revision": "02aa1a334e54a8f9d84b",
+    "url": "/getpurecure/static/css/main.d989985d.chunk.css"
+  },
+  {
+    "revision": "de059b5a66707bcfa89e",
+    "url": "/getpurecure/static/js/2.a19015b4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/getpurecure/static/js/2.a19015b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02aa1a334e54a8f9d84b",
+    "url": "/getpurecure/static/js/main.02a788d1.chunk.js"
+  },
+  {
+    "revision": "9f32d0307ea262384534",
+    "url": "/getpurecure/static/js/runtime-main.86e8d607.js"
+  }
+]);
