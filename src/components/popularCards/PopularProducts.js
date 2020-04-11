04abb38551c2a014c3dff4ed382d403d4fd9b7d6
Faisal Haque Bappy <hbfaisal66@gmail.com>
2020-04-12 01:28:14 +0600

    Done 11th

--- src/components/popularCards/PopularProducts.js
+++ src/components/popularCards/PopularProducts.js
@@ -1,152 +1,204 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function PopularProducts(props) {
-  return (
-    <div className={props.size}>
-      <div
-        className="mm-hospital-card"
-        data-toggle="modal"
-        data-target={"#example" + props.content.url}
-      >
-        <div className="mm-card-content mt-3">
-          <h4 className="text-dark text-center">Popular Products</h4>
+import axios from "axios";
+import URLs from "../../URLs";
 
-          <ul class="list-group list-group-flush bg-white px-4 py-4">
-            <li class="list-group-item  bg-white">
-              <img
-                src="https://madebyloop.co.uk/images/products/slider/memphis/memphis-pattern-animated.gif"
-                alt={""}
-                width="60px"
-                height="60px"
-                className="rounded-circle mr-2"
-              />
+export default class PopularProducts extends React.Component {
+  constructor(props) {
+    super(props);
 
-              <b className="text-dark mr-1 text-lg">Product Name</b>
-              <small className="text-dark ">৳1200</small>
-            </li>
-            <li class="list-group-item  bg-white">
-              <img
-                src="https://madebyloop.co.uk/images/products/slider/memphis/memphis-pattern-animated.gif"
-                alt={""}
-                width="60px"
-                height="60px"
-                className="rounded-circle mr-2"
-              />
+    this.state = {
+      recent_blogs: [],
+    };
+  }
 
-              <b className="text-dark mr-1 text-lg">Product Name</b>
-              <small className="text-dark ">৳1200</small>
-            </li>
-            <li class="list-group-item  bg-white">
-              <img
-                src="https://madebyloop.co.uk/images/products/slider/memphis/memphis-pattern-animated.gif"
-                alt={""}
-                width="60px"
-                height="60px"
-                className="rounded-circle mr-2"
-              />
+  componentDidMount() {
+    axios
+      .get(URLs.shop_api + "products/all/recent")
+      .then((response) => {
+        if (response.status === 200) {
+          this.setState({
+            recent_blogs: response.data,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("error " + error);
+      });
+  }
 
-              <b className="text-dark mr-1 text-lg">Product Name</b>
-              <small className="text-dark ">৳1200</small>
-            </li>
-
-            <div className="row justify-content-center mt-3">
-              <Link
-                to="/getpurecure/shop"
-                type="button center"
-                className="btn btn-accent"
-              >
-                See More
-              </Link>
-            </div>
-          </ul>
-        </div>
-      </div>
-
-      <div
-        class="modal fade "
-        id={"example" + props.content.url}
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalCenterTitle"
-        aria-hidden="true"
-      >
+  render() {
+    return (
+      <div className={this.props.size}>
         <div
-          class="modal-dialog modal-dialog-centered modal-lg"
-          role="document"
+          className="mm-hospital-card"
+          data-toggle="modal"
+          data-target={"#example" + this.props.content.url}
         >
-          <div class="modal-content">
-            <div class="modal-header">
-              <h3 class="modal-title" id="exampleModalLongTitle">
-                {props.content.title}
-              </h3>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <div className="row">
-                <img
-                  className="col-md-12 modal-image"
-                  src={props.content.top_img || "./logo.svg"}
-                  alt={""}
-                />
-              </div>
-              <br />
-              <ul>
-                {props.content.summary.split(/\n|।/).map((line) => (
-                  <li>{line}</li>
-                ))}
-              </ul>
-              <br />
-
-              {props.content.publish_date !== null ? (
-                <div className="ml-4">
-                  {new Date(
-                    Date.parse(props.content.publish_date)
-                  ).toLocaleDateString("bn-BD", {
-                    weekday: "long",
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  })}
-                </div>
-              ) : (
-                <div />
-              )}
-              <br />
-              <div className="mm-card-footer ml-4">
-                <div>
+          <div className="mm-card-content mt-3">
+            <h4 className="text-dark text-center">Popular Products</h4>
+            {this.state.recent_blogs.length >= 3 ? (
+              <ul class="list-group list-group-flush bg-white px-4 py-4">
+                <li class="list-group-item  bg-white">
                   <img
-                    className="newspaper-icon"
                     src={
-                      "https://s2.googleusercontent.com/s2/favicons?domain_url=" +
-                      props.content.source_url
+                      this.state.recent_blogs[0].photos[0] ||
+                      "https://madebyloop.co.uk/images/products/slider/memphis/memphis-pattern-animated.gif"
                     }
-                    alt="icon"
+                    alt={""}
+                    width="60px"
+                    height="60px"
+                    className="rounded-circle mr-2"
                   />
-                  {" " + props.content.source_url.split(".")[1]}
+
+                  <b className="text-dark mr-1 text-lg">
+                    {this.state.recent_blogs[0].name}
+                  </b>
+                  <small className="text-dark ">
+                    ৳{this.state.recent_blogs[0].price}
+                  </small>
+                </li>
+                <li class="list-group-item  bg-white">
+                  <img
+                    src={
+                      this.state.recent_blogs[1].photos[0] ||
+                      "https://madebyloop.co.uk/images/products/slider/memphis/memphis-pattern-animated.gif"
+                    }
+                    alt={""}
+                    width="60px"
+                    height="60px"
+                    className="rounded-circle mr-2"
+                  />
+
+                  <b className="text-dark mr-1 text-lg">
+                    {this.state.recent_blogs[1].name}
+                  </b>
+                  <small className="text-dark ">
+                    ৳{this.state.recent_blogs[1].price}
+                  </small>
+                </li>
+                <li class="list-group-item  bg-white">
+                  <img
+                    src={
+                      this.state.recent_blogs[2].photos[0] ||
+                      "https://madebyloop.co.uk/images/products/slider/memphis/memphis-pattern-animated.gif"
+                    }
+                    alt={""}
+                    width="60px"
+                    height="60px"
+                    className="rounded-circle mr-2"
+                  />
+
+                  <b className="text-dark mr-1 text-lg">
+                    {this.state.recent_blogs[2].name}
+                  </b>
+                  <small className="text-dark ">
+                    ৳{this.state.recent_blogs[2].price}
+                  </small>
+                </li>
+
+                <div className="row justify-content-center mt-3">
+                  <Link
+                    to="/getpurecure/shop"
+                    type="button center"
+                    className="btn btn-accent"
+                  >
+                    See More
+                  </Link>
                 </div>
-              </div>
-              <div className="row justify-content-center">
-                <a
-                  href={props.content.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  type="button center"
-                  className="btn btn-success btn-accent text-white"
+              </ul>
+            ) : (
+              <div />
+            )}
+          </div>
+        </div>
+
+        <div
+          class="modal fade "
+          id={"example" + this.props.content.url}
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalCenterTitle"
+          aria-hidden="true"
+        >
+          <div
+            class="modal-dialog modal-dialog-centered modal-lg"
+            role="document"
+          >
+            <div class="modal-content">
+              <div class="modal-header">
+                <h3 class="modal-title" id="exampleModalLongTitle">
+                  {this.props.content.title}
+                </h3>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
                 >
-                  সম্পুর্ন সংবাদ দেখুন
-                </a>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div className="row">
+                  <img
+                    className="col-md-12 modal-image"
+                    src={this.props.content.top_img || "./logo.svg"}
+                    alt={""}
+                  />
+                </div>
+                <br />
+                <ul>
+                  {this.props.content.summary.split(/\n|।/).map((line) => (
+                    <li>{line}</li>
+                  ))}
+                </ul>
+                <br />
+
+                {this.props.content.publish_date !== null ? (
+                  <div className="ml-4">
+                    {new Date(
+                      Date.parse(this.props.content.publish_date)
+                    ).toLocaleDateString("bn-BD", {
+                      weekday: "long",
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </div>
+                ) : (
+                  <div />
+                )}
+                <br />
+                <div className="mm-card-footer ml-4">
+                  <div>
+                    <img
+                      className="newspaper-icon"
+                      src={
+                        "https://s2.googleusercontent.com/s2/favicons?domain_url=" +
+                        this.props.content.source_url
+                      }
+                      alt="icon"
+                    />
+                    {" " + this.props.content.source_url.split(".")[1]}
+                  </div>
+                </div>
+                <div className="row justify-content-center">
+                  <a
+                    href={this.props.content.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    type="button center"
+                    className="btn btn-success btn-accent text-white"
+                  >
+                    সম্পুর্ন সংবাদ দেখুন
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
