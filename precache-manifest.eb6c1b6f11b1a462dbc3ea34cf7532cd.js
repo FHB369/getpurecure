a2ff769626521dded20034ace2c052dd69af81af
Faisal Haque Bappy <hbfaisal66@gmail.com>
2020-04-15 13:12:16 +0600

    Updates

---
+++ precache-manifest.eb6c1b6f11b1a462dbc3ea34cf7532cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba51e4b62eddee1b58fa6acd54d7212e",
+    "url": "/getpurecure/index.html"
+  },
+  {
+    "revision": "ef647181af948cb3d754",
+    "url": "/getpurecure/static/css/main.858d54bd.chunk.css"
+  },
+  {
+    "revision": "e5f8ba6602b5425cff5d",
+    "url": "/getpurecure/static/js/2.d0298616.chunk.js"
+  },
+  {
+    "revision": "b5e7381e844126614de8d30e2ed0d240",
+    "url": "/getpurecure/static/js/2.d0298616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef647181af948cb3d754",
+    "url": "/getpurecure/static/js/main.2f581e86.chunk.js"
+  },
+  {
+    "revision": "9f32d0307ea262384534",
+    "url": "/getpurecure/static/js/runtime-main.86e8d607.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/getpurecure/precache-manifest.184d78e905bffdba1aa6b35319ca09b6.js"
+  "/getpurecure/precache-manifest.eb6c1b6f11b1a462dbc3ea34cf7532cd.js"
 );
 
 self.addEventListener('message', (event) => {
